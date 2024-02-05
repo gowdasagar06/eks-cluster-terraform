4c08737308ee419ab596f8ae4265352b1b586f55
gowdasagar06 <92599815+gowdasagar06@users.noreply.github.com>
2024-02-06 01:34:04 +0530

    Update ci-print-dependencies.sh

--- ci-print-dependencies.sh
+++ ci-print-dependencies.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+sudo apt update
+curl -LO https://releases.hashicorp.com/terraform/1.1.0/terraform_1.1.0_linux_amd64.zip
+sudo apt install unzip
+unzip terraform_*.zip
+sudo mv terraform /usr/local/bin/
+sudo apt install awscli
 
 # Make the script to abort if any command fails
 set -e
