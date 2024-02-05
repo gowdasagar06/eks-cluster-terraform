c4c254ef421f0bdf6c47cdaf2321c6f8ad39ed60
gowdasagar06 <92599815+gowdasagar06@users.noreply.github.com>
2024-02-05 22:45:24 +0530

    Create ci-terraform-init-plan-apply.sh

---
+++ ci-terraform-init-plan-apply.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Make the script to abort if any command fails
+set -e
+
+# Print the commands as it is executed. Useful for debugging
+set -x
+
+terraform -chdir=environments/$ENV_NAME init -reconfigure -input=false
+
+terraform -chdir=environments/$ENV_NAME plan -input=false -var-file="../../variables/$ENV_NAME.tfvars"
+
+terraform -chdir=environments/$ENV_NAME apply -auto-approve -input=false -var database_password=$TF_VAR_database_password -var-file="../../variables/$ENV_NAME.tfvars"
