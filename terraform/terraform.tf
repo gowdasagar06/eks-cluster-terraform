eca7fb4cee67eafdad6a79e9cffaceac0db8565c
gowdasagar06 <92599815+gowdasagar06@users.noreply.github.com>
2024-02-12 23:47:44 +0530

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-bucket-ekscluster" 
+    bucket         = "elasticbeanstalk-ap-south-1-814257489889" 
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
